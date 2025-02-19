25c3dceff4abf080b38346e72c8aabfa3b6e7765
Artem V. Tretiak <artem.tretiak@partech.com>
2025-02-19 18:41:59 +0200

    feat: add semver as a release generator

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: [
+    { name: 'main' },
+    { name: 'release', prerelease: 'rc' }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    // '@semantic-release/changelog',
+    ['@semantic-release/git', {
+      // assets: ['CHANGELOG.md', 'package.json', '*.csproj'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]'
+    }],
+    '@semantic-release/github'
+  ]
+};
